278d991a8abe00123e94ea60f6f9eb33cc16b116
Per-Olov Jernberg <possan@possan.se>
2014-10-22 23:45:17 +0200

    Unit tests \!

--- tests/units/controller.js
+++ tests/units/controller.js
@@ -1,6 +1,10 @@
 var chai = require('chai');
 var Sky = require('../../lib/sky');
+var Worker = require('../../lib/worker');
+var stream = require('stream');
 var expect = chai.expect;
+
+console.log('Sky', Sky);
 
 describe('pipeline', function () {
 
@@ -10,21 +14,41 @@ describe('pipeline', function () {
   var worker3;
   var pipeline;
 
-  beforeEach(function () {
+  beforeEach(function() {
+    console.log('be');
     env = Sky.env('local');
-    worker1 = new Sky.Worker('worker1');
-    worker2 = new Sky.Worker('worker2');
-    worker3 = new Sky.Worker('worker3');
+    worker1 = new Worker(null, 'worker1');
+    worker2 = new Worker(null, 'worker2');
+    worker3 = new Worker(null, 'worker3');
+    console.log('be2');
     pipeline = worker1.pipe(worker2).pipe(worker3);
+    console.log('be3');
   });
-  
-  describe('ping', function (done) {
-    worker1.emit({__control: 'PING'});
-    pipeline.process(function(item){
-      expect(item).to.exist;
-      expect(item.chain).to.exist;
-      done();
-    })
+
+  describe('ping', function() {
+    it('gets ping', function(done) {
+      pipeline.process = function(item) {
+        console.log('end of pipeline', item);
+        expect(item).to.exist;
+        done();
+      }
+      worker1.emit({ __control: 'PING' });
+    });
+
+    it('gets complete route', function(done) {
+      pipeline.process = function(item) {
+        console.log('end of pipeline 2', item);
+        expect(item).to.exist;
+        expect(item.__chain).to.exist;
+        expect(item.__chain.length).to.equal(3);
+        expect(item.__chain).to.eql([
+          worker1.id,
+          worker2.id,
+          worker3.id]);
+        done();
+      }
+      worker1.emit({ __control: 'PING' });
+    });
   });
 
   describe('connect', function () {
